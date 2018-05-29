fd1bf8ac27e2229b46cd29f692499fd25f37de2b
mhuihuia <373248213@qq.com>
2018-05-29 21:36:44 +0800

    修复学习页面bug

--- exam/urls.py
+++ exam/urls.py
@@ -6,7 +6,7 @@ app_name = 'exam'
 urlpatterns = [
     path('tip/', views.gettip, name='tip'),
     path('index/', views.exam, name='exam'),
-    path('examResult/', views.getresult, name='examResult'),
+    path('examResult/', views.getresult, name='getresult'),
     path('getdone/', views.getdone, name='getdone'),
 
 ]
--- exam/views.py
+++ exam/views.py
@@ -2,15 +2,24 @@ from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
-from page.models import Rule
+from page.models import Rule, Page
 
 
 def  gettip(request):
     return render(request, 'exam_tip.html')
 
 def exam(request):
+    page = Page.objects.all()[1]
+    rule_list = Rule.objects.filter(implemented=1)[:7]
+    rule = rule_list[0]
     list = map(str, range(25))
-    return render(request, 'exam.html', {'list':list})
+    context = {
+        'page': page,
+        'rule_list': rule_list,
+        'rule': rule,
+        'list': list,
+    }
+    return render(request, 'exam.html', context)
 
   # //初始化页面时将已学习过的条目字体设为红色
 def  getdone(request):
