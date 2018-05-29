719e46460d460bfaca5e8158c8517749118dcd81
mhuihuia <373248213@qq.com>
2018-05-29 13:23:49 +0800

    修复前端学习页面bug

--- learningsystem/views/views.py
+++ learningsystem/views/views.py
@@ -6,6 +6,9 @@ from django.shortcuts import render
 from page.models import Rule,Page
 
 
+def  loading_iframe(request):
+    return render(request, 'iframe.html')
+
 def  ruleList(request):
     rule_list = Rule.objects.filter(implemented=1).order_by('rule_id')
     context = {
@@ -15,33 +18,20 @@ def  ruleList(request):
 
 # 获取用户学习开始前选择的rule
 ruleids=[]
-def  getRuleIds(request):
-     global ruleids
-     if request.method == 'POST':
-        user_obj = json.loads(request.body.decode())
-        ruleids = user_obj.get('ruleIds')
-        resultstatus = 'sucess'
-        if len(ruleids)!=0:
-            return JsonResponse(resultstatus, safe=False)
 
 def  study(request):
+    ruleids =request.POST.getlist('checkchild')
     print(ruleids)
     page = Page.objects.all()[1]
     rule_list = Rule.objects.filter(implemented=1)[:7]
-    rule = Rule.objects.get(pk = 4)
+    rule = rule_list[0]
     context = {
         'page': page,
         'rule_list': rule_list,
         'rule': rule,
     }
-    print(context.get('page'))
     return render(request, 'study_task.html', context)
 
-def  loading_iframe(request):
-    return render(request, 'iframe.html')
 
 
-#考试
-def exam(request):
-    list = map(str, range(25))
-    return render(request, 'exam.html', {'list':list})
+
