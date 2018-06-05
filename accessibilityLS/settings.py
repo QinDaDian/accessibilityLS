e058e8ceedd3c9a418d15c067aa970e1ddd78864
qindian <416101605@qq.com>
2018-06-05 16:05:38 +0800

    session改进

--- accessibilityLS/settings.py
+++ accessibilityLS/settings.py
@@ -133,3 +133,13 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=(os.path.join(BASE_DIR, 'static/'),)
 
 MEDIA_ROOT=os.path.join(BASE_DIR,"static/media").replace('\\', '/')
+
+SESSION_COOKIE_NAME="eagle"  # Session的cookie保存在浏览器上时的key，即：sessionid＝随机字符串
+SESSION_COOKIE_PATH="/"  # Session的cookie保存的路径
+SESSION_COOKIE_DOMAIN = None  # Session的cookie保存的域名
+SESSION_COOKIE_SECURE = False  # 是否Https传输cookie
+SESSION_COOKIE_HTTPONLY = True  # 是否Session的cookie只支持http传输
+SESSION_COOKIE_AGE = 86400  # Session的cookie失效日期（2周） 默认1209600秒
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True  # 是否关闭浏览器使得Session过期
+
+SESSION_SAVE_EVERY_REQUEST = True  # 如果设置 True，在30分钟期间有请求服务端，就不会过期
--- learningsystem/views.py
+++ learningsystem/views.py
@@ -7,15 +7,13 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
 import random
-
 from accessibilityLS import settings
 from page.models import Rule, Page
 from learningsystem.models import Item, Record
 from utils.decorator import authentication
 
 
-IMGs = []   #上传的图片
-ruleids=[]
+# 获取用户学习开始前选择的rule
 @authentication
 def ruleList(request):
     rule_list = Rule.objects.filter(implemented=1).order_by('rule_id')
@@ -23,14 +21,16 @@ def ruleList(request):
         'rule_list': rule_list,
     }
     return render(request, 'rule_list.html', context)
-# 获取用户学习开始前选择的rule
+
 
 
 @authentication
 def study(request):
-    global ruleids
     if request.method=="POST":
-       ruleids=request.POST.getlist('checkchild')
+        ruleids = request.POST.getlist('checkchild')
+        request.session['ruleids'] = ruleids
+    else:
+        ruleids = request.session.get('ruleids')
     # 规则筛选
     pages = Page.objects.all()
     page = random.sample(list(pages),7)
@@ -56,24 +56,23 @@ def loading_iframe(request):
 @csrf_exempt
 @authentication
 def submit_learn(request):
-    global IMGs
-    imgs = ','.join(IMGs)
+    imgs = ','.join(request.session.get('IMGs'))
     if request.is_ajax():
         arg = json.loads(request.body.decode('utf-8'))
         item = Item.objects.filter(page_id=arg['pageID'], rule_id=arg['ruleID'])
         print(arg["start_time"])     # 2018/6/4 21:40:24
-        start_time =datetime.strptime(arg["start_time"], "%Y/%m/%d %H:%M:%S")
-        start_time =datetime.strftime(start_time, "%Y-%m-%d %H:%M:%S")
+        start_time = datetime.strptime(arg["start_time"], "%Y/%m/%d %H:%M:%S")
+        start_time = datetime.strftime(start_time, "%Y-%m-%d %H:%M:%S")
         if item:
             std_result = item.result
-            text_reason=item.text_reason
+            text_reason = item.text_reason
         else:
             std_result = 2
             text_reason = '不存在'
             Record.objects.create(
                 page_id=arg['pageID'],
                 rule_id=arg['ruleID'],
-                user_id=1,
+                user_id=request.session.get('username'),
                 std_result=std_result,
                 user_result=arg['userResult'],
                 reason=arg['userReason'],
@@ -82,13 +81,16 @@ def submit_learn(request):
                 judge=1 if std_result == arg['userResult'] else 0,
                 start_time=start_time,
                 create_time=start_time,
-        )
+            )
+            # 存下图片路径后删除session
+            del request.session['IMGs']
         result = {
             "stdReason": text_reason,
             "std_result": std_result,
-            'resultStatus': 'SUCESS'
+            'resultStatus': 'SUCCESS'
         }
     return JsonResponse(result, safe=False)
+
 
 # 切换学习项
 def change_item(request):
@@ -107,19 +109,26 @@ def change_item(request):
 
 # //图片上传
 @csrf_exempt
+@authentication
 def swfUpload(request):
-    global IMGs
     upload_file = request.FILES.get('file')
     file_suffix = upload_file.name.split(".")[-1]  #后缀
     curr_time = datetime.now().strftime("%Y%m%d%H%M%S%f") #获取当前时间
     newName = 'upload/'+ curr_time + '.'+file_suffix
-    IMGs.append(newName)
     fname = '%s/%s' % (settings.MEDIA_ROOT, newName)
     with open(fname, 'wb') as pic:
         for f in upload_file.chunks():
             pic.write(f)
         pic.close()
-    return HttpResponse("sucess")
+
+    # 图片路径存入session
+    IMGs = request.session.get('IMGs', False)
+    if IMGs is False:
+        IMGs = [newName]
+    else:
+        IMGs.append(newName)
+    request.session['IMGs'] = IMGs
+    return HttpResponse("success")
 
 
 def tojson(queryset):
